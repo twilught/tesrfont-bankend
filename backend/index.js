37f3ae1d0466d54ce80c7493e103457d87ef8f29
Athip Buasamlee <athipbuasamlee@MacBook-Air-khxng-Athip-3.local>
2025-05-21 00:48:11 +0700

    Fix: clean app router and config for render

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api/hello', (req, res) => {
-    res.json({ message: "มายรักก้อง" });
+    res.json({ message: "Hello from Express Backend!" });
 });
 
 app.listen(PORT, () => {
